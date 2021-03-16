1f1ccd9a812019e121f1a948c6670bf02ded197b
hoekstradev <hoekstradev@gmail.com>
2021-03-16 15:06:05 -0400

    Add CSV output capability

--- ProfitRankingCal.py
+++ ProfitRankingCal.py
@@ -7,6 +7,7 @@ from os import path
 import time
 # import requests
 import json
+import csv
 import webbrowser
 import urllib.parse
 import threading
@@ -16,8 +17,11 @@ exportPath = "C:\\Users\\Dallas H\\Downloads"
 exportFilename = '\\collatedData.json'
 keywordList = "C:\\Users\\Dallas H\\Downloads\\keywordList.txt"
 
-# extract the list of comma separated keywords to collect data on
-# when I switch to a GUI input system I may need to change the parse method
+export_to_json = False
+export_to_csv = True
+
+# extract the list of comma separated keywords to collect data on.
+# If I switch to a GUI input system I may need to change the parse method.
 def parseKeywordList():
     with open(keywordList) as keywordFile:
         keywordString = ''.join(keywordFile.readlines())
@@ -50,7 +54,7 @@ def collateData(numFiles):
         print(f'opening {filename}')
         with open(filename) as json_file:
             data = json.load(json_file)
-            keywordTraffic[data['keyword']] = data['keywordVolumeByRank']
+            keywordTraffic[data['keyword']] = data['keywordVolumeByRank']    
         os.remove(filename)
         #             print(sourceFiles)
         # print(len(sourceFiles))
@@ -58,8 +62,37 @@ def collateData(numFiles):
 
 def exportCollatedData(keywordTraffic):
     exportFile = exportPath + exportFilename
-    with open(exportFile, 'w') as outfile:
-        json.dump(keywordTraffic, outfile)
+
+    
+    if (export_to_json):
+        if path.exists(exportFile):
+            os.remove(exportFile)
+
+        with open(exportFile, 'w') as outfile:
+            json.dump(keywordTraffic, outfile)
+    elif (export_to_csv):
+        exportFile = exportFile.split('.')[0]
+        exportFile = exportFile + ".csv"
+
+        if path.exists(exportFile):
+            os.remove(exportFile)
+
+        with open(exportFile, 'w') as outfile:
+            csv_writer = csv.writer(outfile)
+            headersWritten = False
+
+            for keyword in keywordTraffic:
+                if not (headersWritten):
+                    header = ["Keyword", "Result 1", "Result 2", "Result3", "Result 4", "Result5", 
+                                "Result 6", "Result 7", "Result 8", "Result 9", "Result 10"]
+                    csv_writer.writerow(header)
+                    headersWritten = True
+                row = [keyword]
+                for rank in keywordTraffic[keyword]:
+                    row.append(rank)
+                csv_writer.writerow(row)
+
+
 
 def main():
     # Collect data on the desired keywords
