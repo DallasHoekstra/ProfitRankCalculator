8fb7c55dbea4be182e6aecc1d876ecec3afff75b
hoekstradev <hoekstradev@gmail.com>
2021-03-15 20:52:16 -0400

    javascript saves keyword-ranking key-value pairs to json. Python collects key-value files from specified folder and places them into a single json file.

--- ChromeExtension/stats_extractor.js
+++ ChromeExtension/stats_extractor.js
@@ -1,35 +1,61 @@
-// window.onload = highlightData("red", "green");
-
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
 var observer = new MutationObserver(function(mutations, observer) {
-    // highlightData("yellow", "blue");
-    collectAndCleanData();
+    keywordVolumeByRank = clicksAtRank();
+    keyword = retrieveKeywords();
+    if (keywordVolumeByRank && keyword) {
+        // localStorage.setItem(keyword, keywordVolumeByRank);
+        dataObject = {
+            "keyword": keyword,
+            "keywordVolumeByRank": keywordVolumeByRank
+        };
+        // prepend filename with KWP- to separate from other files for easy automated processing
+        filename = "KWP-" + keyword;
 
-    // console.log(mutations, observer);
+        downloadKeywordAndRankData(dataObject, filename);
+    }
+
 });
 
-// define what element should be observed by the observer
-// and what types of mutations trigger the callback
 observer.observe(document, {
   subtree: true,
   childList: true
 });
 
-function highlightData(color1, color2) {
-    var firstHref = $("span").css("background-color", color1);
-    firstHref.children().css("background-color", color2);
-    
+function retrieveKeywords() {
+    keywordTag = document.getElementsByTagName("input")[0];
+    if (keywordTag.value !== '') {
+        return keywordTag.value;
+    } 
+}
+
+function clicksAtRank() {
+    cleanedData = collectAndCleanData();
+
+    if (cleanedData != false) {
+        avgMonthlyVolume = parseInt(cleanedData.Volume, 10);
+
+        percentAtRank = [.3802, .1246, .0767, .0506, .0144, .0163, .0145, .0106, .01, .0113];
+        volumeAtRank = [];
+        for (i = 0; i < percentAtRank.length; i++) {
+            volumeAtRank.push(Math.trunc(avgMonthlyVolume*percentAtRank[i]));
+        }
+        return volumeAtRank;
+    }
+
+    return false;
 }
 
 function collectAndCleanData() {
     statsBar = document.getElementById("xt-info");
     
-    // console.log(statsBar != null)
     if (statsBar != null) {
-
         extractedStats = extractStatsBar(statsBar);
-        processedStats = processStats(extractedStats);
+        processedData = processStats(extractedStats);
+        return processedData;
+    }
+    else {
+        return false;
     }
 }
 
@@ -43,9 +69,15 @@ function processStats(extractedStats) {
         
     categories = extractedStats.split("|")
 
-    avgMonthlyVolume = categories[0].split(":"); // "Volume: [number]/month"
-    avgMonthlyVolume = avgMonthlyVolume[1].split("/"); // [number]/month
-    avgMonthlyVolume = avgMonthlyVolume[0]; // [number]
+    avgMonthlyVolume = categories[0].split(":"); // "Volume: [number,number]/month"
+    avgMonthlyVolume = avgMonthlyVolume[1].split("/"); // [number,number]/month
+    avgMonthlyVolume = avgMonthlyVolume[0]; // [number,number]
+    avgMonthlyVolume = avgMonthlyVolume.split(",");
+    avgMonthlyVolume[0] = parseInt(avgMonthlyVolume[0]);
+    while (avgMonthlyVolume.length > 1) {
+        avgMonthlyVolume[0] = avgMonthlyVolume[0]*1000;
+        avgMonthlyVolume.pop();
+    }
 
     cpc = categories[1].split(":") // "CPC: [dollars.cents]"
     cpc = cpc[1] // "[dollars.cents]"
@@ -60,4 +92,21 @@ function processStats(extractedStats) {
     };
 
     return processedData;
+}
+
+
+function highlightData(color1, color2) {
+    var firstHref = $("span").css("background-color", color1);
+    firstHref.children().css("background-color", color2);
+    
+}
+
+function downloadKeywordAndRankData(data, filename){
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(data));
+    var autoDownloadLink = document.createElement('a');
+    autoDownloadLink.setAttribute("href", dataStr);
+    autoDownloadLink.setAttribute("download", filename + ".json");
+    document.body.appendChild(autoDownloadLink); // required for firefox
+    autoDownloadLink.click();
+    autoDownloadLink.remove();
 }
--- ProfitRankingCal.py
+++ ProfitRankingCal.py
@@ -1,21 +1,63 @@
 # A script to collect site data on the potential value of a google ranking position to a web company with some conversion-to-profit calculation
 import sys
+import os
 import requests
 from bs4 import BeautifulSoup
 import time
-import re
+import requests
+import json
+import webbrowser
+
+pathToDownloads = "C:\\Users\\Dallas H\\Downloads"
+exportPath = "C:\\Users\\Dallas H\\Downloads"
+exportFilename = '\\collatedData.json'
+
+def autoSearchKeywords(keywords):
+    webbrowser.get("open -a C:\\Program Files(x86)\\Google\\Chrome\\Application\\chrome.exe")
+    webbrowser.open
+    #https://www.google.com/search?q=open+browser+tab+python&oq=open+browser+tab+python&aqs=chrome..69i57j0i22i30l3.5183j0j7&sourceid=chrome&ie=UTF-8
+    for keyword in keywords:
+        webbrowser.open(keyword, 2)
+
+def collateData(sourceFiles):
+    keywordTraffic = {}
+
+    for filename in sourceFiles:
+        print(filename)
+        filename = pathToDownloads + "\\" + filename
+        print(filename)
+        with open(filename) as json_file:
+            data = json.load(json_file)
+            keywordTraffic[data['keyword']] = data['keywordVolumeByRank']
+    return keywordTraffic
+
+def exportCollatedData(keywordTraffic):
+    exportFile = exportPath + exportFilename
+    with open(exportFile, 'w') as outfile:
+        json.dump(keywordTraffic, outfile)
+
+def main():
+    
+    autoSearchKeywords("covid relief mortgage")
+
+    keywordFiles = [filename for filename in os.listdir(pathToDownloads) if (filename.startswith("KWP-") & filename.endswith(".json"))]
+    keywordTraffic = collateData(keywordFiles)
+    exportCollatedData(keywordTraffic)
+
+
+main()
 
 
 
 
 
-percentagePage = requests.get('https://www.advancedwebranking.com/ctrstudy')
-print(percentagePage)
-parsedwebpage = BeautifulSoup(percentagePage.content, 'html.parser')
-print(parsedwebpage)
-rankColumns = parsedwebpage.find_all('g', class_="amcharts-graph-column") #, aria-label=True
+# percentagePage = requests.get('https://www.advancedwebranking.com/ctrstudy')
+# print(percentagePage)
+# parsedwebpage = BeautifulSoup(percentagePage.content, 'html.parser')
+# print(parsedwebpage)
+# rankColumns = parsedwebpage.find_all('g', class_="amcharts-graph-column") #, aria-label=True
 
-print("lalalalalal")
-for column in rankColumns:
-    print(column)
+# print("lalalalalal")
+# for column in rankColumns:
+#     print(column)
 
